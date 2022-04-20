cbedfebb217ec0d6a29de3cc936c1547ef312354
Allan Kley <allanlkley@gmail.com>
2022-04-20 11:08:28 -0300

    sei la o que ta acontecene do aqui

---
+++ Velha/WebApplication2/WebApplication2/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApplication2
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuração e serviços de API Web
+
+            // Rotas de API Web
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "Login Route",
+                routeTemplate: "api/login/{userName}/{senha}",
+                new {
+                    controller = "Player",
+                    userName = String.Empty,
+                    senha = String.Empty,
+                }
+            );
+        }
+    }
+}
---
+++ Velha/WebApplication2/WebApplication2/Controllers/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApplication2.Controllers
+{
+    public class PlayerController : ApiController
+    {
+        // GET: api/Player
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Player/5
+        public object Get(string userName, string senha)
+        {
+            
+            return "value";
+        }
+
+        // POST: api/Player
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Player/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Player/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
